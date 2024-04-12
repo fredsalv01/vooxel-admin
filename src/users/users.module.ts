4bfac49463dfbd3eed64aac5bcd04676c90d4dd9
Freddy Morales <xt7957@rimac.com.pe>
2024-04-12 12:47:17 -0500

    config changes and user module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../auth/entities/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [UsersService],
+  controllers: [],
 })
 export class UsersModule {}
-  
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/createUserDto.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  async register(createUserDto: CreateUserDto) {
+    const data = createUserDto;
+    return `User ${data.firstName} has been registered`;
+  }
+}
