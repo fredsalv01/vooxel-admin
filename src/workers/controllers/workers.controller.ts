ad1550b98e4db11273afe98f6461846913c309e9
Freddy Morales <freddy.morales@usil.pe>
2024-04-21 14:35:45 -0500

    workers routes ensure

--- src/workers/controllers/workers.controller.ts
+++ src/workers/controllers/workers.controller.ts
@@ -13,12 +13,14 @@ import {
   ClassSerializerInterceptor,
   UseInterceptors,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { WorkersService } from '../services/workers.service';
 import { CreateWorkerDto } from '../dto/create-worker.dto';
 import { UpdateWorkerDto } from '../dto/update-worker.dto';
 import { PaginationDto } from '../../pagination/dto/pagination.dto';
 import { filterWorkersPaginatedDto } from '../dto/filter-get-workers.dto';
+import { AuthGuardJwt } from '../../auth/guards/auth-guard-jwt.guard';
 
 @Controller('workers')
 @SerializeOptions({ strategy: 'exposeAll' })
@@ -27,31 +29,45 @@ export class WorkersController {
   constructor(private readonly workersService: WorkersService) {}
 
   @Post()
+  @UseGuards(AuthGuardJwt)
   create(@Body() createWorkerDto: CreateWorkerDto) {
+    this.logger.log(this.create.name);
+    this.logger.debug('body', createWorkerDto);
     return this.workersService.create(createWorkerDto);
   }
 
   @Get()
+  @UseGuards(AuthGuardJwt)
   @UsePipes(new ValidationPipe({ transform: true }))
   @UseInterceptors(ClassSerializerInterceptor)
-  findAll(
-    @Query() queryParams: filterWorkersPaginatedDto
-  ) {
+  findAll(@Query() queryParams: filterWorkersPaginatedDto) {
+    this.logger.log(this.findAll.name);
+    this.logger.debug('query', queryParams);
     return this.workersService.findAll(queryParams);
   }
 
   @Get(':id')
+  @UseGuards(AuthGuardJwt)
   findOne(@Param('id') id: string) {
+    this.logger.log(this.findOne.name);
+    this.logger.debug('urlParams', id);
     return this.workersService.findOne(+id);
   }
 
   @Patch(':id')
+  @UseGuards(AuthGuardJwt)
   update(@Param('id') id: string, @Body() updateWorkerDto: UpdateWorkerDto) {
+    this.logger.log(this.update.name);
+    this.logger.debug('urlParams', id);
+    this.logger.debug('body', updateWorkerDto);
     return this.workersService.update(+id, updateWorkerDto);
   }
 
   @Delete(':id')
+  @UseGuards(AuthGuardJwt)
   remove(@Param('id') id: string) {
+    this.logger.log(this.remove.name);
+    this.logger.debug('urlParams', id);
     return this.workersService.remove(+id);
   }
 }
