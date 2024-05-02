972603be67d79ba7d18b9fcb66a98fcbf85a9f11
fredsalv01 <freddy.morales@usil.pe>
2024-05-02 06:09:27 -0500

    feat: limpiando archivos que no se usan

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import ormConfig from './config/orm.config';
@@ -28,8 +26,8 @@ import { ClientsModule } from './clients/clients.module';
     WorkersModule,
     ClientsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
   exports: [AuthModule],
 })
 export class AppModule {}
