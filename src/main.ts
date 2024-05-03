54275fc258b1eb8443505ca76a902a58237b0ed8
Freddy Morales <xt7957@rimac.com.pe>
2024-05-03 13:44:13 -0500

    changes on port

--- src/main.ts
+++ src/main.ts
@@ -31,7 +31,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  
-  await app.listen(port);
+
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
