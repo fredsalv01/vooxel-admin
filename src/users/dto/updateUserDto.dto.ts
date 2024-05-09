d5c73bbeacf70e6cbbc7263173b16c3faa57c3e1
fredsalv01 <freddy.morales@usil.pe>
2024-05-08 23:50:43 -0500

    validation update users

--- src/users/dto/updateUserDto.dto.ts
+++ src/users/dto/updateUserDto.dto.ts
@@ -1,18 +1,41 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './createUserDto.dto';
-import { IsEmail, Length } from 'class-validator';
+import {
+  IsBoolean,
+  IsEmail,
+  IsOptional,
+  IsString,
+  Length,
+} from 'class-validator';
 import { isUnique } from '../../validation/isUnique.constraint';
+import { Transform } from 'class-transformer';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
   @Length(5)
   @isUnique({ tableName: 'user', column: 'username', method: 'update' })
   username?: string;
+
+  @IsString()
   firstName?: string;
+
+  @IsString()
   lastName?: string;
+
   @IsEmail()
   @isUnique({ tableName: 'user', column: 'email', method: 'update' })
   email?: string;
+
+  @IsString()
+  @IsOptional()
   password?: string;
+
+  @IsString()
+  @IsOptional()
   retypedPassword?: string;
+
+  @IsBoolean()
+  @Transform(({ obj, key }) => {
+    return obj[key] === 'true' ? true : obj[key] === 'false' ? false : obj[key];
+  })
   isActive?: boolean;
 }
