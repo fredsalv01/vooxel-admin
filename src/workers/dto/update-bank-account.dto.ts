4b09d4d4fe78d901abf4c5b675fd23a6726fdc97
fredsalv01 <freddy.morales@usil.pe>
2024-05-26 15:53:11 -0500

    fix: update bank account dto

--- src/workers/dto/update-bank-account.dto.ts
+++ src/workers/dto/update-bank-account.dto.ts
@@ -1,19 +1,37 @@
 import { IsBoolean, IsEnum, IsOptional } from 'class-validator';
 import { BankAccountTypes, BankNames } from '../utils/enum-types';
+import { ApiProperty } from '@nestjs/swagger';
+import { getRandomInt } from '../../common/functions';
 
 export class UpdateBankAccountDto {
+  @ApiProperty({
+    description: 'Nombre del banco de la cuenta',
+    example: `${BankNames.BANCO_LA_NACION}`,
+  })
   @IsEnum(BankNames, {
     message: `Tipos de bancos válidos: ${BankNames}`,
   })
   @IsOptional()
   bankName?: BankNames;
 
+  @ApiProperty({
+    description: 'Codigo de cuenta interbancario',
+    example: `${getRandomInt(111111111111111111111, 999999999999999999999)}`,
+  })
   @IsOptional()
   cci?: string;
 
+  @ApiProperty({
+    description: 'Codigo de cuenta',
+    example: `${getRandomInt(1111111111111, 9999999999999)}`,
+  })
   @IsOptional()
   bankAccountNumber?: string;
 
+  @ApiProperty({
+    description: 'Tipo de cuenta bancaria',
+    example: `${BankAccountTypes.AHORROS}`,
+  })
   @IsEnum(BankAccountTypes, {
     message: `Tipos de cuenta de banco válidas: ${BankAccountTypes}`,
   })
