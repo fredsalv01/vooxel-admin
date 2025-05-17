e3e0186bd89a274eeffda25de24928855183b96c
Freddy Morales <_jgdonzalezg@falabella.com.pe>
2025-05-17 17:29:27 -0500

    bucket credentials config

--- libs/bucket/src/bucket.service.ts
+++ libs/bucket/src/bucket.service.ts
@@ -10,10 +10,23 @@ export class BucketService {
   constructor() {
     const GCP_PROJECT_ID = 'file-upload-app-b9ae4'; // reemplaza
     const GCP_KEY_FILE_PATH = 'gcp-app.json'; // REEMPLAZAR
+    const credentials = {
+      "type": process.env.GCP_TYPE,
+      "project_id": process.env.GCP_PROJECT_ID,
+      "private_key_id": process.env.GCP_PRIVATE_KEY_ID,
+      "private_key": process.env.GCP_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      "client_email": process.env.GCP_CLIENT_EMAIL,
+      "client_id": process.env.GCP_CLIENT_ID,
+      "auth_uri": process.env.GCP_AUTH_URI,
+      "token_uri": process.env.GCP_TOKEN_URI,
+      "auth_provider_x509_cert_url": process.env.GCP_AUTH_PROVIDER_CERT_URL,
+      "client_x509_cert_url": process.env.GCP_CLIENT_CERT_URL,
+      "universe_domain": process.env.UNIVERSE_DOMAIN
+    }
     this.bucketName = 'staging.file-upload-app-b9ae4.appspot.com'; // REEMPLAZAR
     this.storage = new Storage({
       projectId: GCP_PROJECT_ID,
-      keyFilename: GCP_KEY_FILE_PATH,
+      credentials
     });
   }
 
